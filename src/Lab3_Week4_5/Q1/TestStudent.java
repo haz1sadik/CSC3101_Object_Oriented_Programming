cee92e70853d5c42ecefe3468d0416f516c29cb6
HazuanTech <hazuantechnology@outlook.com>
2024-04-18 08:08:32 +0800

    update

--- src/Lab3_Week4_5/Q1/TestStudent.java
+++ src/Lab3_Week4_5/Q1/TestStudent.java
@@ -38,45 +38,5 @@ public class TestStudent {
 
         System.out.println(undergradStudent);
         System.out.println(postGradstudent);
-        /*System.out.print("Enter Student Matric Number : ");
-        student.setMatricNum(input.nextInt());
-        input.nextLine();
-        System.out.print("Enter Student Name : ");
-        student.setName(input.nextLine());
-        System.out.print("Enter Student Email : ");
-        student.setEmail(input.nextLine());
-        System.out.print("Enter Student GPA : ");
-        student.setGpa(input.nextDouble());
-
-        System.out.print("Enter Undergraduate Student Matric Number : ");
-        undergraduate1.setMatricNum(input.nextInt());
-        input.nextLine();
-        System.out.print("Enter Undergraduate Student Name : ");
-        undergraduate1.setName(input.nextLine());
-        System.out.print("Enter Undergraduate Student Email : ");
-        undergraduate1.setEmail(input.nextLine());
-        System.out.print("Enter Undergraduate Student GPA : ");
-        undergraduate1.setGpa(input.nextDouble());
-        input.nextLine();
-        System.out.print("Enter Undergraduate Student Classification : ");
-        undergraduate1.setClassification(input.next());
-
-        System.out.print("\nEnter Postgraduate Student Matric Number : ");
-        postgraduate1.setMatricNum(input.nextInt());
-        input.nextLine();
-        System.out.print("Enter Postgraduate Student Name : ");
-        postgraduate1.setName(input.nextLine());
-        System.out.print("Enter Postgraduate Student Name : ");
-        postgraduate1.setEmail(input.nextLine());
-        System.out.print("Enter Postgraduate Student GPA : ");
-        postgraduate1.setGpa(input.nextDouble());
-        input.nextLine();
-        System.out.print("Enter Undergraduate Student Thesis Name : ");
-        postgraduate1.setThesisName(input.next());
-
-        System.out.println(student);
-        System.out.println(undergraduate1);
-        System.out.println(postgraduate1);
-         */
     }
 }
